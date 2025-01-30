8f6a2734fe10495e9888d58c49afbcb5bb3ade3a
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-01-30 23:22:17 +0530

    Update IndexPage.js

--- client/src/Components/IndexPage.js
+++ client/src/Components/IndexPage.js
@@ -587,7 +587,7 @@ export default function IndexPage() {
                     {
                         token ?
                             <span>{userName}</span>
-                            : <>GLANCE</>
+                            : <>Vigilance/SECR</>
                     }
                 </logo>
 
