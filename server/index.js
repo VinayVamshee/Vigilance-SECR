b075efca107d34e4db2015d6422700370b652cdb
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-02-12 20:38:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.post('/admin/login', async (req, res) => {
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
-        const token = jwt.sign({ id: adminUser._id, username: adminUser.username }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: adminUser._id, username: adminUser.username }, JWT_SECRET);
         res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         res.status(500).json({ message: 'Login failed', error });
