efda2157c38c11c54a0b0ede3ec73123c1dce028
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-02-12 22:16:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import IndexPage from "./Components/IndexPage";
-import Documentation from "./Components/Documentation"; // Import Documentation component
+import Documentation from "./Components/Documentation"; 
 import './Components/style.css';
 
 function App() {
